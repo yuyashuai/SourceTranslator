22d9fd0d85e7c2fed5d371e2a519dff9ffd82ea6
yuyashuai <yuyashuai@gmail.com>
2017-09-13 14:10:19 +0800

    initial commit

---
+++ src/Config.java
@@ -0,0 +1,18 @@
+public class Config {
+
+    /**
+     * 百度相关api key，免费的用完就没有了，可以个人免费申请
+     */
+    public static final String BD_APP_ID = "20170830000079162";
+    public static final String BD_APP_SEC = "9a6_lGJertdYrT67iKKQ";
+    /**
+     * 有道相关api key，免费的用完就没有了，可以个人免费申请
+     */
+    public static final String YD_APP_ID="7a943f3b3665de16";
+    public static final String YD_APP_KEY="P3njQNi5TPgCHZthRZvmPa9yZqzbm2Yg";
+
+    //param1:word ,param2:samples(optional)
+    public static final String BING_URL = "http://xtk.azurewebsites.net/BingDictService.aspx";
+    public static final String BD_URL = "https://fanyi-api.baidu.com/api/trans/vip/translate";
+    public static final String YD_URL = "https://openapi.youdao.com/api";
+}
